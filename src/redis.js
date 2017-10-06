340d7a7e831f43332404e0b91f282c53fde96be9
Abhisek Pattnaik <abhisekp@engineer.com>
2017-10-07 00:41:28 +0530

    Add redis setup

---
+++ src/redis.js
@@ -0,0 +1,7 @@
+import redis from 'redis';
+import bluebird from 'bluebird';
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+export default redis;
